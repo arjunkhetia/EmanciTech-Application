9d911356b1909f2a598d27cad6637aa3a74e7ffd
Arjun Khetia <arjunkhetia@gmail.com>
2024-02-18 20:01:15 +0530

    Application Icon + Splash Screen Done

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  useEffect(() => {
+    setTimeout(async () => {
+      // This tells the splash screen to hide immediately!
+      await SplashScreen.hideAsync();
+    }, 2000);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>EmanciTech Application</Text>
